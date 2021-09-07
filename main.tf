06502bc35fe69d8bfbb731f42156d4c83c6bc9af
pathaknikhil87 <53004363+pathaknikhil87@users.noreply.github.com>
2021-09-07 11:43:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "first_module" {
+module "module-nikvm" {
   source  = "app.terraform.io/pathaknikhil87/terraform-aws-nikvm"
   version = "v1.0.2"
 }
