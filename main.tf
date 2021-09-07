7a48c9f7d4b5fdd0f6c803a2a7b54ac8211baddc
pathaknikhil87 <53004363+pathaknikhil87@users.noreply.github.com>
2021-09-07 11:58:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,36 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "nikvm" {
-  source  = "app.terraform.io/Labnik-6sept21/nikvm/aws"
-  version = "1.0.4"
+variable "username" {
+ type = string
+ default = "gds"
+}
+variable "i_type" {
+ type = string
+ default = "t2.micro"
+}
+variable "ami" {
+ type = string
+ default = "ami-087c17d1fe0178315"
+}
+
+variable "key" {
+ type = string
+ default = "gagan-test-key"
+}
+
+resource "aws_instance" "myawsserver" {
+  ami = var.ami
+  instance_type = var.i_type
+#  key_name = var.key
+#  user_data = templatefile("/home/gagan/terraform/aws/first_demo/user_data.tftpl", { user = var.username})
+
+  tags = {
+    Name = upper("Gagandeep-aws-ec2-instance")
+    Env = lower("Dev")
+  }
+}
+
+output "myawsserver-ip" {
+  value = "${aws_instance.myawsserver.public_ip}"
 }
